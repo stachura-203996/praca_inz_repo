cedece71537e0cf71ccffcb8b8e4ce28a2e22fe1
Tomasz Stachura <203996@edu.p.lodz.pl>
2018-11-01 21:55:18 +0100

    Init project

---
+++ praca_inz/praca_inz-app/src/main/java/com/stachura/praca_inz/backend/PracaInzApplication.java
@@ -0,0 +1,12 @@
+package com.stachura.praca_inz.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaInzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracaInzApplication.class, args);
+	}
+}
