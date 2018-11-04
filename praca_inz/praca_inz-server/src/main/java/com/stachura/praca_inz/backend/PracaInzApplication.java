7ca98858c734f84f03a9b6c92d3bc918bae05d5d
Tomasz Stachura <203996@edu.p.lodz.pl>
2018-11-04 15:24:45 +0100

    Solving issues with showing frontend

--- praca_inz/praca_inz-server/src/main/java/com/stachura/praca_inz/backend/PracaInzApplication.java
+++ praca_inz/praca_inz-server/src/main/java/com/stachura/praca_inz/backend/PracaInzApplication.java
@@ -2,11 +2,12 @@ package com.stachura.praca_inz.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PracaInzApplication {
+public class PracaInzApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PracaInzApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PracaInzApplication.class, args);
+    }
 }
